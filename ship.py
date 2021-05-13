ef0403fc1f60a7d2fab21e342aa3fb7ce427a62b
Max Liles <lileswm@miamioh.edu>
2021-05-13 16:42:05 -0400

    added movement to ship

--- ship.py
+++ ship.py
@@ -14,6 +14,14 @@ class Ship:
 
         # Start each new ship at bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement flag
+        self.moving_right = False
+
+    def update(self):
+        """ Update the ship's position based on Movement Flag """
+        if self.moving_right:
+            self.rect.x += 1    
     
     def blitme(self):
         """Draw the ship at its current location"""
